e348004412d9f695f9c9e83aef7a2f2f83109491
diosaraiva <diosaraiva@192.168.0.13>
2024-03-09 17:15:35 -0300

    new repo.

---
+++ src/test/java/com/diosaraiva/SpringContextTest.java
@@ -0,0 +1,13 @@
+package com.diosaraiva;
+
+import org.junit.Test;
+
+import com.diosaraiva.camel.App;
+
+public class SpringContextTest {
+
+	@Test
+    public final void testMain() throws Exception {
+        App.main(null);
+	}
+}
